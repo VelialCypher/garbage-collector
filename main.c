e66cc89f033153130a7a32ebe7b604b4348ebd99
VelialCypherÐ <stug_kt@mail.ru>
2021-02-21 17:58:21 +0300

    feat: add a methods for mark unused objects

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdbool.h>
 
 #define STACK_MAX 256
 
@@ -11,7 +12,7 @@ typedef enum {
 
 typedef struct Object {
 	ObjectType_t type;
-
+	bool marked;
 	union {
 		int value;
 		struct {
@@ -23,23 +24,23 @@ typedef struct Object {
 
 typedef struct {
 	Object_t* stack[STACK_MAX];
-	int size;
+	int stackSize;
 } VM_t;
 
 VM_t* VM_new() {
 	VM_t* vm = (VM_t*)malloc(sizeof(VM_t));
-	vm -> size = 0;
+	vm -> stackSize = 0;
 	return vm;
 }
 
 void VM_pushInStack(VM_t* vm, Object_t* item) {
-	assert(vm -> size < STACK_MAX);
-	vm -> stack[vm -> size++] = item;
+	assert(vm -> stackSize < STACK_MAX);
+	vm -> stack[vm -> stackSize++] = item;
 }
 
 Object_t* VM_popFromStack(VM_t* vm) {
-	assert(vm -> size > 0);
-	return vm -> stack[--vm -> size];
+	assert(vm -> stackSize > 0);
+	return vm -> stack[--vm -> stackSize];
 }
 
 Object_t* Object_new(VM_t* vm, ObjectType_t* type) {
@@ -61,6 +62,23 @@ Object_t* VM_pushPair(VM_t* vm) {
 
 	VM_pushInStack(vm, obj);
 	return obj;
+}
+
+void Object_mark(Object_t* obj) {
+	if(obj -> marked) return;
+
+	obj -> marked = true;
+
+	if(obj -> type == OBJ_PAIR) {
+		Object_mark(obj -> head);
+		Object_mark(obj -> tail);
+	}
+}
+
+void VM_markAllObjInStack(VM_t* vm) {
+	for (size_t i = 0; i < vm -> stackSize; i++) {
+		Object_mark(vm -> stack[i]);
+	}
 }
 
 int main() {
